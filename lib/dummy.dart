12ba85dd9ec91dda690f399704cd15a0b9ff6079
fajarmuhammadsidiq <fajars328@gmail.com>
2023-02-14 23:09:43 +0700

    commit pertama

---
+++ lib/dummy.dart
@@ -0,0 +1,6 @@
+class DataPekerajaan {
+  List<Map<String, dynamic>> data = [
+    {"nama": "Andri", "Alamat": "Depok", "Pekerjaan": "Wiraswasta"},
+    {"nama": "Andra", "Alamat": "Jakarta", "Pekerjaan": "Wiraswasta"}
+  ];
+}
---
+++ lib/homapage.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:test_app/dummy.dart';
+
+class Homepage extends StatelessWidget {
+  Homepage({super.key});
+  DataPekerajaan data = DataPekerajaan();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("App"),
+      ),
+      body: ListView(   
+      ),
+    );
+  }
+}
